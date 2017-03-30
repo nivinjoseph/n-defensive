cd00cc8cc7a73eb110851af9eb7080a66c497697
Amanda Furzer <amanda.furzer@P15017-AFurzer-10-4-101-28.wifi-4.stjosephcontent.com>
2017-03-30 14:34:35 -0400

    finished implementing tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,70 +1,308 @@
 import * as assert from "assert";
 import given from "../src/index";
 import "n-ext";
-import { Exception, ArgumentException, ArgumentNullException } from "n-exception";
+import { Exception, ArgumentException, ArgumentNullException} from "n-exception";
 
-suite("Replace me", () =>
+suite("Exceptions thrown", () =>
 {
-    suite("given", () =>
+    let arg: any;
+    let argName: any;
+    let exceptionHappened: boolean;
+    let exceptionType: string;
+    let reason: any;  
+    
+    setup(() =>
     {
-        let arg: string;
-        let argName: any;
-        let exceptionHappend: boolean;
-        let exceptionType: string;
-        
-        setup(() =>
-        {
-            arg = "arg";
-            argName = null;
-            exceptionHappend = false;
-            exceptionType
-
+        arg = null;
+        argName = null;
+        exceptionHappened = false;
+        reason = null;
+    });
+    
+    suite("given", () =>
+    {               
+        test("should throw an ArgumentNullException if argName (second arg) is null", () => 
+        {      
             try 
             {
                 given(arg, argName);
             }
             catch (exp)
             {
-                exceptionHappend = true;
+                exceptionHappened = true;
                 exceptionType = (<Object>exp).getTypeName();
-            } 
-        });
-        
-        test("should throw an ArgumentNullException if argName (second arg) is null", () => 
-        {
-            
-            
-            assert.strictEqual(exceptionHappend, true);
+            }
+            assert.strictEqual(exceptionHappened, true);
             assert.strictEqual(exceptionType, "ArgumentNullException");
         });
         
-        test("should throw an ArgumentNullException if argName (second arg) is undefined");
-        test("should throw an ArgumentNullException if argName (second arg) is an empty string");
-        test("should throw an ArgumentNullException if argName (second arg) is an whitespace string");
-        test("given should return an Ensurer object");
+        test("should throw an ArgumentNullException if argName (second arg) is undefined", () =>
+        {
+            argName = undefined;
+            try 
+            {
+                given(arg, argName);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentNullException");
+        });
+        
+        test("should throw an ArgumentNullException if argName (second arg) is an empty string", () =>
+        {
+            argName = "";
+            try 
+            {
+                given(arg, argName);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentNullException");
+        });
+        
+        test("should throw an ArgumentNullException if argName (second arg) is an whitespace string", () =>
+        {
+            argName = "  ";
+            try 
+            {
+                given(arg, argName);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentNullException");
+        });
+        
+        test("given should return an Ensurer object", () =>
+        {
+            let ensurer = given({}, "argName");
+            try 
+            {
+                given(arg, argName);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.ok(ensurer != null);
+        });
     });
     
-    suite("ensureHasValue", () => 
-    {
-        test("should throw ArgumentNullException if arg is null");
-        test("should throw ArgumentNullException if arg is undefined");
-        test("should not throw any exceptions if arg has value");
+    suite("ensureHasValue", () =>     
+    {        
+        test("should throw ArgumentNullException if arg is null", () =>
+        {          
+            try 
+            {
+                given(arg, argName).ensureHasValue();
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }             
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentNullException");            
+        }); 
+        
+        test("should throw ArgumentNullException if arg is undefined", () =>
+        {
+            arg = undefined;
+            try 
+            {
+                given(arg, argName).ensureHasValue();
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }             
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentNullException");
+        }); 
+        
+        test("should not throw any exceptions if arg has value", () =>
+        {
+            arg = "arg";
+            argName = "argName";
+            try 
+            {
+                given(arg, argName).ensureHasValue();
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            } 
+            assert.strictEqual(exceptionHappened, false); 
+        });
     });
     
     suite("ensure", () =>
     {
-        test("should throw ArgumentNullException if func is null");
-        test("should throw ArgumentNullException if func is undefined");
-        test("should throw InvalidArgumentException if func returns false");
-        test("should not throw any exceptions if the func returns true");
+        setup(() =>
+        {
+            arg = "arg";
+            argName = "argName";
+        });
+        
+        test("should throw ArgumentNullException if func is null", () =>
+        {            
+            try 
+            {
+                given(arg, argName).ensure(null, "reason");
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentNullException");            
+        });
+        
+        test("should throw ArgumentNullException if func is undefined", () =>
+        {
+            try 
+            {
+                given(arg, argName).ensure(undefined, "reason");
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentNullException");   
+        });
+        
+        test("should throw InvalidArgumentException if func returns false", () =>
+        {
+            try 
+            {
+                given(arg, argName).ensure(arg => false);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "InvalidArgumentException");   
+        });
+        
+        test("should not throw any exceptions if the func returns true", () =>
+        {
+            try 
+            {
+                given(arg, argName).ensure(arg => true, "reason");
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, false);
+        });
     });
     
     suite("ensure with reason", () =>
     {
-        test("should throw ArgumentException if func returns false and reason is null");
-        test("should throw ArgumentException if func returns false and reason is undefined");
-        test("should throw ArgumentException if func returns false and reason is empty string");
-        test("should throw ArgumentException if func returns false and reason is whitespace string");
-        test("should throw InvalidArgumentException if func returns false and reason is a valid string");
+        setup(() =>
+        {
+            arg = "arg";
+            argName = "argName";
+        });
+        
+        test("should throw InvalidArgumentException if func returns false and reason is null", () =>
+        {
+            try 
+            {
+                given(arg, argName).ensure(arg => false, reason);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "InvalidArgumentException");      
+        });
+        
+        test("should throw InvalidArgumentException if func returns false and reason is undefined", () =>
+        {
+            reason = undefined;
+            try 
+            {
+                given(arg, argName).ensure(arg => false, reason);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "InvalidArgumentException");
+        });
+        
+        test("should throw InvalidArgumentException if func returns false and reason is empty string", () =>
+        {
+            reason = "";
+            try 
+            {
+                given(arg, argName).ensure(arg => false, reason);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "InvalidArgumentException");    
+        });
+        
+        test("should throw InvalidArgumentException if func returns false and reason is whitespace string", () =>
+        {
+            reason = "  ";
+            try 
+            {
+                given(arg, argName).ensure(arg => false, reason);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "InvalidArgumentException");     
+        });
+        
+        test("should throw ArgumentException if func returns false and reason is a valid string", () =>
+        {
+            reason = "reason";
+            try 
+            {
+                given(arg, argName).ensure(arg => false, reason);
+            }
+            catch (exp)
+            {
+                exceptionHappened = true;
+                exceptionType = (<Object>exp).getTypeName();
+            }
+            assert.strictEqual(exceptionHappened, true);
+            assert.strictEqual(exceptionType, "ArgumentException");  
+        });
     });
 });
