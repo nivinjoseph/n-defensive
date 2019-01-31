79d410d18f6b528e574b4ff8a792751c06ef55b4
nivinjoseph <nivinjoseph@outlook.com>
2019-01-31 07:43:28 -0500

    preparing to publish new version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,34 @@
 import "@nivinjoseph/n-ext";
 export interface Ensurer<T> {
     ensureHasValue(): Ensurer<T>;
-    ensureIsString(): Ensurer<T>;
-    ensureIsNumber(): Ensurer<T>;
-    ensureIsBoolean(): Ensurer<T>;
-    ensureIsObject(): Ensurer<T>;
-    ensureIsFunction(): Ensurer<T>;
-    ensureIsArray(): Ensurer<T>;
-    ensureIsType(type: Function): Ensurer<T>;
-    ensureHasStructure(structure: object): Ensurer<T>;
     ensure(func: (arg: T) => boolean): Ensurer<T>;
     ensure(func: (arg: T) => boolean, reason: string): Ensurer<T>;
 }
-export declare function given<T>(arg: T, argName: string): Ensurer<T>;
+export interface StringEnsurer extends Ensurer<string> {
+    ensureIsString(): StringEnsurer;
+}
+export interface NumberEnsurer extends Ensurer<number> {
+    ensureIsNumber(): NumberEnsurer;
+}
+export interface BooleanEnsurer extends Ensurer<boolean> {
+    ensureIsBoolean(): BooleanEnsurer;
+}
+export interface ArrayEnsurer extends Ensurer<Array<any>> {
+    ensureIsArray(): ArrayEnsurer;
+}
+export interface FunctionEnsurer extends Ensurer<Function> {
+    ensureIsFunction(): FunctionEnsurer;
+}
+export interface ObjectEnsurer extends Ensurer<object> {
+    ensureIsObject(): ObjectEnsurer;
+    ensureIsType(type: Function): ObjectEnsurer;
+    ensureHasStructure(structure: object): ObjectEnsurer;
+}
+declare function given(arg: string, argName: string): StringEnsurer;
+declare function given(arg: number, argName: string): NumberEnsurer;
+declare function given(arg: boolean, argName: string): BooleanEnsurer;
+declare function given(arg: Array<any>, argName: string): ArrayEnsurer;
+declare function given(arg: Function, argName: string): FunctionEnsurer;
+declare function given(arg: object, argName: string): ObjectEnsurer;
+declare function given<T, U extends Ensurer<T>>(arg: T, argName: string): U;
+export { given };
