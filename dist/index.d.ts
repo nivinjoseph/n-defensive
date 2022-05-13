7d1ec34a418bdf281ce79d569d47cc4f7e2cb41c
nivinjoseph <nivinjoseph@outlook.com>
2022-05-13 06:30:47 -0400

    preparing to publish new version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,35 +1,35 @@
 import "@nivinjoseph/n-ext";
 export interface Ensurer<T> {
-    ensureHasValue(when?: boolean | (() => boolean)): this;
-    ensure(func: (arg: T) => boolean): this;
-    ensure(func: (arg: T) => boolean, reason: string): this;
-    ensureWhen(when: boolean | (() => boolean), func: (arg: T) => boolean): this;
-    ensureWhen(when: boolean | (() => boolean), func: (arg: T) => boolean, reason: string): this;
+    ensureHasValue(when?: boolean | (() => boolean)): this | never;
+    ensure(func: (arg: T) => boolean): this | never;
+    ensure(func: (arg: T) => boolean, reason: string): this | never;
+    ensureWhen(when: boolean | (() => boolean), func: (arg: T) => boolean): this | never;
+    ensureWhen(when: boolean | (() => boolean), func: (arg: T) => boolean, reason: string): this | never;
 }
 export interface StringEnsurer extends Ensurer<string> {
-    ensureIsString(when?: boolean | (() => boolean)): this;
-    ensureIsEnum(enumType: object, when?: boolean | (() => boolean)): this;
+    ensureIsString(when?: boolean | (() => boolean)): this | never;
+    ensureIsEnum(enumType: object, when?: boolean | (() => boolean)): this | never;
 }
 export interface NumberEnsurer extends Ensurer<number> {
-    ensureIsNumber(when?: boolean | (() => boolean)): this;
-    ensureIsEnum(enumType: object, when?: boolean | (() => boolean)): this;
+    ensureIsNumber(when?: boolean | (() => boolean)): this | never;
+    ensureIsEnum(enumType: object, when?: boolean | (() => boolean)): this | never;
 }
 export interface BooleanEnsurer extends Ensurer<boolean> {
-    ensureIsBoolean(when?: boolean | (() => boolean)): this;
+    ensureIsBoolean(when?: boolean | (() => boolean)): this | never;
 }
 export interface ArrayEnsurer<TItem> extends Ensurer<ReadonlyArray<TItem>> {
-    ensureIsArray(when?: boolean | (() => boolean)): this;
+    ensureIsArray(when?: boolean | (() => boolean)): this | never;
 }
 export interface FunctionEnsurer extends Ensurer<Function> {
-    ensureIsFunction(when?: boolean | (() => boolean)): this;
+    ensureIsFunction(when?: boolean | (() => boolean)): this | never;
 }
 export interface ObjectEnsurer<T extends object> extends Ensurer<T> {
-    ensureIsObject(when?: boolean | (() => boolean)): this;
-    ensureIsType(type: new (...args: any[]) => T, when?: boolean | (() => boolean)): this;
+    ensureIsObject(when?: boolean | (() => boolean)): this | never;
+    ensureIsType(type: new (...args: any[]) => T, when?: boolean | (() => boolean)): this | never;
     ensureIsInstanceOf(type: Function & {
         prototype: T;
-    }, when?: boolean | (() => boolean)): this;
-    ensureHasStructure(structure: object, when?: boolean | (() => boolean)): this;
+    }, when?: boolean | (() => boolean)): this | never;
+    ensureHasStructure(structure: object, when?: boolean | (() => boolean)): this | never;
 }
 declare function given(arg: string, argName: string): StringEnsurer;
 declare function given(arg: number, argName: string): NumberEnsurer;
